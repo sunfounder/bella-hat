faee8b5852a0975b238bfba22ff1fcdfbe8925c6
Lovmong <1967471283@qq.com>
2025-05-15 17:27:26 +0800

    fix install

--- certificate/install.sh
+++ certificate/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt install -y bluez iw iperf3 rfkill bluez-hcidump
+sudo apt install -y bluez iw iperf3 rfkill bluez-hcidump screen
 
 cd ~/bella-hat
 
@@ -10,8 +10,8 @@ rm -rf .venv
 python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
 
-cd ~/bella-hat/vilib
-sudo python3 install.py 
+# cd ~/bella-hat/vilib
+# sudo python3 install.py 
 
 cd ~/bella-hat
 pip install ./
